29462b9ed67ef1981c5602563fc325880448b631
Tony_Huang <luck938938@hotmail.com>
2019-06-04 17:05:44 -0400

    addBinary

--- index.js
+++ index.js
@@ -26,3 +26,12 @@ function maskify(cc) {
         return cc;
     }
 }
+
+
+// Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+// The binary number returned should be a string.
+
+function addBinary(a, b) {
+    return (a+b).toString(2);
+ }
