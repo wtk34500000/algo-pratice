476ce48fbb62a13b72651303afd2e71550777244
Tony_Huang <luck938938@hotmail.com>
2019-06-06 14:26:37 -0400

    add more problems

---
+++ Format a string of names like 'Bart, Lisa & Maggie'./answer.js
@@ -0,0 +1,13 @@
+function list(names){
+    //your code here
+    if(names.length > 1){
+      let str=[]
+     for(let i =0; i< names.length - 1; i++){
+      str.push(names[i].name)
+     }
+   
+     return str.join(', ')+" & "+names[names.length-1].name
+    }
+   
+     return names[0]? names[0].name: ''
+}
---
+++ Sort the odd/answer.js
@@ -0,0 +1,7 @@
+function sortArray(array) {
+    //filter out the odd and sort it
+    let oddArr=array.filter(num => num % 2 !==0).sort((a, b) => a - b)
+    
+    //map through the orginal array and replace the odd number with the sorted odd num from the oddArr
+    return array.map(num => num % 2 ===1 ?  oddArr.shift() : num )
+  }
