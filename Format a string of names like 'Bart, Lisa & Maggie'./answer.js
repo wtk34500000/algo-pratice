e13162b53e7bb3909d780b62355e2f7aaa20931b
Tony_Huang <luck938938@hotmail.com>
2019-06-07 22:47:14 -0400

    add more problems

--- Format a string of names like 'Bart, Lisa & Maggie'./answer.js
+++ Format a string of names like 'Bart, Lisa & Maggie'./answer.js
@@ -2,12 +2,10 @@ function list(names){
     //your code here
     if(names.length > 1){
       let str=[]
-     for(let i =0; i< names.length - 1; i++){
+      for(let i =0; i< names.length - 1; i++){
       str.push(names[i].name)
-     }
-   
-     return str.join(', ')+" & "+names[names.length-1].name
+      }
+      return str.join(', ')+" & "+names[names.length-1].name
     }
-   
-     return names[0]? names[0].name: ''
+    return names[0]? names[0].name: ''
 }
