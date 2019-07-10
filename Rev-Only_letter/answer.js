aeb7a5ef2046bbc364a1e5db67389995d26fa6e9
Tony_Huang <luck938938@hotmail.com>
2019-07-09 21:04:20 -0400

    add more problems

---
+++ Rev-Only_letter/answer.js
@@ -0,0 +1,19 @@
+var reverseOnlyLetters = function(S) {
+    const letter=[]
+    let result=""
+    for(let i =0; i<S.length; i ++){
+        if(/^[a-zA-Z]+$/.test(S[i])){
+            letter.unshift(S[i])
+        }
+    }
+    
+    for(let i =0; i<S.length; i ++){
+        if(/^[a-zA-Z]+$/.test(S[i])){
+           result=result+letter.shift()
+        }else{
+            result+=S[i]
+        }
+    }
+    
+    return result;
+};
