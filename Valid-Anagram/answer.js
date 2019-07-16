6f176c47ebcadee793f61e0ee192dba1adfee080
Tony_Huang <luck938938@hotmail.com>
2019-07-15 20:43:55 -0400

    add more problems

---
+++ Valid-Anagram/answer.js
@@ -0,0 +1,10 @@
+var isAnagram = function(s, t) {
+    if(s.length!=t.length){
+        return false;
+    }
+    if(s.split("").sort().join("") === t.split("").sort().join("")){
+        return true;
+    }else{
+        return false;
+    }
+};
