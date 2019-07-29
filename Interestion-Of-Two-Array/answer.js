52901f1f4c6443816a578d40eabb706bad25db16
Tony_Huang <luck938938@hotmail.com>
2019-07-29 10:36:01 -0400

    add more problems

---
+++ Interestion-Of-Two-Array/answer.js
@@ -0,0 +1,11 @@
+var intersection = function(nums1, nums2) {
+    let result=[]
+    
+    for(let i = 0; i < nums2.length; i++){
+        if(nums1.includes(nums2[i])){
+            result.push(nums2[i])
+        }
+    }
+    
+    return [...new Set(result)]
+};
