955cb71108b9320072626d3799aca8d0d299b5b0
Tony_Huang <luck938938@hotmail.com>
2019-08-17 23:51:55 -0400

    add more problems

--- Search-In-Binary-Tree/answer.js
+++ Search-In-Binary-Tree/answer.js
@@ -10,6 +10,8 @@
  * @param {number} val
  * @return {TreeNode}
  */
+
+
 var searchBST = function (root, val) {
     if (!root || root.val === val) {
         return root
@@ -21,4 +23,14 @@ var searchBST = function (root, val) {
     if (root.val > val) {
         return searchBST(root.left, val)
     }
+};
+
+
+
+var searchBST = function (root, val) {
+    while (root !== null && root.val !== val) {
+        root = root.val < val ? root.right : root.left
+    }
+
+    return root
 };
