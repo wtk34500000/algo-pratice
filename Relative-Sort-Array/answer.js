706889d06fa91020d870005d8bde594cc7f73525
Tony_Huang <luck938938@hotmail.com>
2019-08-18 23:46:10 -0400

    add more problems

---
+++ Relative-Sort-Array/answer.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @return {number[]}
+ */
+var relativeSortArray = function (arr1, arr2) {
+    let resultPart1 = [],
+        resultPart2 = [],
+        base = arr1,
+        distinct = arr2;
+
+    for (let i = 0; i < base.length; i++) {
+        if (!distinct.includes(base[i])) {
+            resultPart2.push(base[i]);
+        } else {
+            resultPart1 = [...resultPart1, ...base.filter((item) => {
+                return item === distinct[i]
+            })]
+        }
+    }
+    return resultPart1.concat(resultPart2.sort((a, b) => {
+        return a - b
+    }))
+};
